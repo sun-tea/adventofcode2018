8b59d10c0a5f412a5079bb3ee813c928e162210c
Xuan-thi Nguyen <xthi.ngu@gmail.com>
2018-12-03 00:30:03 +0100

    day 1 part 1

---
+++ src/day1.js
@@ -0,0 +1,17 @@
+import fs from 'fs';
+import path from 'path';
+
+const parseFile = (filePath: string = 'input.day1.txt') => {
+  const file = fs.readFileSync(path.resolve('ínputs', filePath), 'utf8');
+
+  const lines = file.split('\n');
+  return lines.reduce((acc, val) =>
+    Number(acc) + Number(val)
+  , 0);
+
+};
+
+(() => {
+  const args = process.argv.slice(2);
+  console.log(parseFile(args[0]));
+})();
