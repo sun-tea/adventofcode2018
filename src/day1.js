18626dabf7d5be22e7e6ae1de40693e8da0eb089
Xuan-thi Nguyen <xthi.ngu@gmail.com>
2018-12-03 01:55:11 +0100

    day 1 part 2

--- src/day1.js
+++ src/day1.js
@@ -1,17 +1,33 @@
+// Day 1: Chronal Calibration
+
 import fs from 'fs';
 import path from 'path';
 
-const parseFile = (filePath: string = 'input.day1.txt') => {
+const parseFile = (filePath: string = 'day1.txt') => {
   const file = fs.readFileSync(path.resolve('inputs', filePath), 'utf8');
+  const content = file.split('\n');
+  console.log(`part1: ${part1(content)}`);
+  console.log(`part2: ${part2(content)}`);
+};
 
-  const lines = file.split('\n');
-  return lines.reduce((acc, val) =>
-    Number(acc) + Number(val)
-  , 0);
+const part1 = (content) => content.reduce((acc, val) => Number(acc) + Number(val), 0);
+const part2 = (content) => {
+  let total = 0;
+  let acc = [ total ];
 
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
+    for (let i = 0; i < content.length; i++) {
+      total += Number(content[i]);
+      if (acc.includes(total)) {
+        return total;
+      }
+      acc.push(total);
+    }
+  }
 };
 
 (() => {
   const args = process.argv.slice(2);
-  console.log(parseFile(args[0]));
+  parseFile(args[0]);
 })();
