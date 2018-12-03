db4d231936b418a7591ea4ae3afe9b0282c37131
Xuan-thi Nguyen <xthi.ngu@gmail.com>
2018-12-03 17:40:49 +0100

    apply linter

--- src/day1.js
+++ src/day1.js
@@ -10,10 +10,11 @@ const parseFile = (filePath: string = 'day1.txt') => {
   console.log(`part2: ${part2(content)}`);
 };
 
-const part1 = (content) => content.reduce((acc, val) => Number(acc) + Number(val), 0);
-const part2 = (content) => {
+const part1 = content =>
+  content.reduce((acc, val) => Number(acc) + Number(val), 0);
+const part2 = content => {
   let total = 0;
-  let acc = [ total ];
+  let acc = [total];
 
   // eslint-disable-next-line no-constant-condition
   while (true) {
