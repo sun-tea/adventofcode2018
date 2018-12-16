ed630df131ca27df045ecb710f8a6c531f97ba91
Xuan-thi Nguyen <xthi.ngu@gmail.com>
2018-12-16 22:25:18 +0100

    day 14 part 2

--- src/day14.ts
+++ src/day14.ts
@@ -19,6 +19,37 @@ const part1 = (content: number) => {
   return scoreboard.slice(content).join('');
 };
 
+const part2 = (content: string) => {
+  const scoreboard = [3, 7];
+  let firstElf = 0;
+  let secondElf = 1;
+  let lastScores = [];
+  let found = false;
+
+  while (!found) {
+    const newRecipe = scoreboard[firstElf] + scoreboard[secondElf];
+    const newItems = newRecipe
+      .toString()
+      .split('')
+      .map((n) => parseInt(n, 10));
+
+    for (let score of newItems) {
+      scoreboard.push(score);
+      lastScores =
+        scoreboard.length > 6 ? scoreboard.slice(scoreboard.length - 6) : [];
+      if (lastScores.length === 6 && lastScores.join('') === content) {
+        found = true;
+        break;
+      }
+    }
+
+    firstElf = (scoreboard[firstElf] + firstElf + 1) % scoreboard.length;
+    secondElf = (scoreboard[secondElf] + secondElf + 1) % scoreboard.length;
+  }
+  return scoreboard.length - 6;
+};
+
 (() => {
   console.log(`part1: ${part1(846601)}`);
+  console.log(`part2: ${part2('846601')}`);
 })();
